67dd081356633e6d0a4423bbd4f19b47dc1d7fe8
sindhulandfox <sindhu.tmg@gmail.com>
2022-09-03 13:17:23 +0530

    hello

---
+++ src/org/cts/Hello.java
@@ -0,0 +1,34 @@
+package org.cts;
+
+public class Hello {
+	public static void main(String[] args) {
+		int Weekdays=6;
+		switch (Weekdays) {
+		case 1:
+			System.out.println("Enter day 1: Monday");
+			break;
+		case 2:
+			System.out.println("Enter day 2: Tuesday");
+			break;
+		case 3:
+			System.out.println("Enter day 3 : Wednesday");
+			break;
+		case 4:
+			System.out.println("Enter day 4 : Thursday");
+			break;
+		case 5:
+			System.out.println("Enter day 5 : Friday");
+			break;
+		case 6:
+			System.out.println("Enter day 6 : Saturday");
+			break;
+		case 7:
+			System.out.println("Enter day 7 : Sunday");
+			break;
+			
+		default:
+			break;
+		}
+	}
+
+}
